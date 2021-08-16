0b12610ea0165bb29ad1a25c165f49e4a2eb1074
Galower <ariosgalower@gmail.com>
2021-08-16 04:59:30 -0400

    added Viro.js and respective component

---
+++ components/Viro.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+export const Viro = () => {
+  return (
+    <View>
+      <Text>Viro page</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({});
